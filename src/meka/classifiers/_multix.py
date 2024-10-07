f6c9a863a431a9ee78ba1796eff082c9916c639d
Peter Reutemann <fracpete@gmail.com>
2024-10-07 18:26:20 +1300

    rechecking boolean flags; fixed super calls

--- src/meka/classifiers/_multix.py
+++ src/meka/classifiers/_multix.py
@@ -1,3 +1,4 @@
+from jpype import JClass
 from weka.classifiers import Classifier
 
 
@@ -5,6 +6,27 @@ class MultiXClassifier(Classifier):
     """
     Common interface for multi-label and multi-target classifiers.
     """
+
+    def __init__(self, classname="weka.classifiers.rules.ZeroR", jobject=None, options=None):
+        """
+        Initializes the specified classifier using either the classname or the supplied JPype object.
+
+        :param classname: the classname of the classifier
+        :type classname: str
+        :param jobject: the JPype object to use
+        :type jobject: JPype object
+        :param options: the list of commandline options to set
+        :type options: list
+        """
+        super().__init__(classname=classname, jobject=jobject, options=options)
+        if not self.is_updateable:
+            self.is_updateable = isinstance(self.jobject, JClass("weka.classifiers.UpdateableClassifier"))
+        if not self.is_drawable:
+            self.is_drawable = isinstance(self.jobject, JClass("weka.core.Drawable"))
+        if not self.is_additional_measure_producer:
+            self.is_additional_measure_producer = isinstance(self.jobject, JClass("weka.core.AdditionalMeasureProducer"))
+        if not self.is_batchpredictor:
+            self.is_batchpredictor = isinstance(self.jobject, JClass("weka.core.BatchPredictor"))
 
     @property
     def model(self) -> str:
